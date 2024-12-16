620a3fe4bdf7a592f517a6c7a1bbd42cc1b27aec
Mahmoud Alawady <m.alawady00@gmail.com>
2024-12-16 14:42:17 +0200

    blog viewer page

---
+++ lib/features/blog/presentation/pages/blog_viewer_page.dart
@@ -0,0 +1,69 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/core/utils/calc_reading_time.dart';
+import 'package:blog_app/core/utils/format_date.dart';
+import 'package:blog_app/features/blog/domain/entities/blog.dart';
+import 'package:flutter/material.dart';
+
+class BlogViewerPage extends StatelessWidget {
+  final Blog blog;
+  const BlogViewerPage({required this.blog, super.key});
+
+  static route(Blog blog) =>
+      MaterialPageRoute(builder: (_) => BlogViewerPage(blog: blog));
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Scrollbar(
+        interactive: true,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  blog.title,
+                  style: const TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'By ${blog.authorName}',
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  '${formatDateByDMMMYYYY(blog.createdAt)} . ${calcReadingTime(blog.content)} min',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: AppPallete.greyColor,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    blog.imageUrl,
+                    errorBuilder: (context, error, stackTrace) =>
+                        const SizedBox(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  blog.content,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    height: 2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
